861b6c8ffcb5e34f89a8dab4d1ec0c9a94849f34
Brandon Abel Solis <solis17@rdev-cl-22-imsl.stanford.edu>
2016-07-06 16:37:57 -0700

    Piping commands into cpp

--- fsdiff-mr/main.cpp
+++ fsdiff-mr/main.cpp
@@ -4,18 +4,27 @@
 //
 //  Created by Brandon Abel Solis on 7/6/16.
 //  Copyright © 2016 Learning Evironments. All rights reserved.
+//  cd /afs/ir.stanford.edu/users/s/o/solis17/Documents/work/fsdiff-mr/DerivedData/fsdiff-mr/Build/Products/Debug
 //
 
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     string command = "ssh root@rdev-mm-51-im27 'ls'";
-    int ret = system(command.c_str());
-    if(ret != 0){
-        cout<<"uh-oh"<<endl;
-        return 0;
+    string command_test = "echo hello";
+    FILE *pipein_ssh = popen(command_test.c_str(), "r");
+    
+    if(pipein_ssh == NULL){
+        perror("popen");
+        exit(1);
     }
-    std::cout << "Hello, World!\n";
+    char readbuf[80];
+    while(fgets(readbuf, 80, pipein_ssh))
+        cout<<"Reading:"<<readbuf<<endl;
+    pclose(pipein_ssh);
+    
+    std::cout << "Done reading\n";
     return 0;
 }
