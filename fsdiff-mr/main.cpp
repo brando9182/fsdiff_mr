bdd95712e5afb691df0489cd2a3b5e5c6bfd0a7f
Brandon Abel Solis <solis17@rdev-cl-22-imsl.stanford.edu>
2016-07-06 15:13:09 -0700

    Initial Commit

---
+++ fsdiff-mr/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fsdiff-mr
+//
+//  Created by Brandon Abel Solis on 7/6/16.
+//  Copyright © 2016 Learning Evironments. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
