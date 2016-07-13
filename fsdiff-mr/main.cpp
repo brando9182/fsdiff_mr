07c35330204287c4cab58ae0009779fb6946772a
Brandon Abel Solis <solis17@rdev-cl-41-imir.stanford.edu>
2016-07-12 17:08:05 -0700

    transcript processing of joe.T\

--- fsdiff-mr/main.cpp
+++ fsdiff-mr/main.cpp
@@ -7,8 +7,11 @@
 /*
  cd /afs/ir.stanford.edu/users/s/o/solis17/Documents/work/fsdiff-mr/DerivedData/fsdiff-mr/Build/Products/Debug
 */
+//TODO: too scp applcationsL
+//Maintain a string for the fille a machine is working on
 
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <vector>
 #include <queue>
@@ -22,6 +25,7 @@ const bool IGNORE_RESPONCE = false;
 struct worker{
     string machine;
     bool busy;
+    string file;
 };
 vector<worker> workers;
 
@@ -51,42 +55,28 @@ queue<string> execute(string command, bool returnList){
     return list;
 }
 
-//Finds the trancript file
-//copies it to master and restores the _original file
-//assumption: the prossesed file made a foo.T an the original is stored as foo_original
-void fetchAndRestore(string machine){
-    string cmdSsh = "ssh "+ machine + " ";
-    string cmdListFiles = "'ls /'";
-    queue<string> files = execute(cmdSsh + cmdListFiles, true);
-    
-    string transcript;
-    while(!files.empty()) {
-        string file = files.front();
-        if(file[file.size()-2] == 'T'){
-            if(vv)cout<<"Found transcript: "<<file;
-            //remove trailing \n
-            transcript = file.substr(0, file.size()-1);
-            break;
-        }
-        files.pop();
+/*
+ *Finds the trancript file
+ *copies it to master and restores the _original file
+ */
+void fetchAndRestore(worker mworker){
+    if(mworker.file == ""){
+        cout<<"Error: "+ mworker.machine + "busy without a file"<<endl;
     }
-    if(transcript.empty()){
-        if(v) cout<<"Error: No transcript found in machine:"<<machine<<endl;
-        return;
-    }
+    string transcript = mworker.file + ".T";
     
-    string cmdCopyTranscript = "scp "+machine+":/"+transcript+" /transcripts";
+    string cmdSsh = "ssh root@" + mworker.machine + " ";
+    string cmdCopyTranscript = "scp "+mworker.machine+":/"+transcript+" /transcripts";
     string cmdRemoveTranscript = "'rm /" + transcript + "'";
     execute(cmdCopyTranscript, IGNORE_RESPONCE);
     execute(cmdSsh + cmdRemoveTranscript, IGNORE_RESPONCE);
     
-    string file = transcript.substr(0, transcript.size()-2);//delete '.T'
-    if(vv) cout<<"Restoring file: "<<file<<endl;
-    string cmdRestorefile = "'mv /" + file + "_original /" + file + "'";
+    if(vv) cout<<"Restoring file: "<<mworker.file<<endl;
+    string cmdRestorefile = "'mv /" + mworker.file + "_original /" + mworker.file + "'";
     execute(cmdSsh + cmdRestorefile, IGNORE_RESPONCE);
 }
-//checks if any worker machine is running fsdiff
-//hangs until it finds one
+
+
 //Multithreading?
 //pro: efficiency++
 //con: complexity++
@@ -98,20 +88,20 @@ worker* findAvailableWorker(){
                 return &mworker;
             }
             
-            //the following may be unreliable. Better to parse the strings and analyze them
-            //possibly pass in an echo or comment to the command to act as sentinel
+            /*query runnig programs to find fsdiff, note 2 false positives from this comand*/
             string cmdSsh = "ssh root@" + mworker.machine + " ";
             string cmdFsdiffCount = "'ps aux | grep fsdiff | wc -l'";
             
             queue<string> result = execute(cmdSsh + cmdFsdiffCount, true);
             if(stoi(result.front()) <= 2){
                 if(vv) cout<<mworker.machine<<" done, fetching transcript and restoring file"<<endl;
-                fetchAndRestore(mworker.machine);
+                fetchAndRestore(mworker);
                 mworker.busy = false;
+                mworker.file = "";
                 return &mworker;
             }
         }
-        if(v) cout<<"No worker is availbale, sleeping for 10 seconds"<<endl;
+        if(vv) cout<<"No worker is availbale, sleeping for 10 seconds"<<endl;
         sleep(10);
     }
 }
@@ -120,36 +110,24 @@ worker* findAvailableWorker(){
 //sends the worker machines files to fsdiff
 //stores their transcript (as filename.T) when they are done
 //and sends another file if there are any left
-void manageWorkers(vector<string> machines){
+void manageWorkers(){
     if(v) cout<<"Getting files at the root directory"<<endl;
     string cmdListFiles = "ls /"; //Should I secify the / directory?
     queue<string> files = execute(cmdListFiles, true);
     if(v) cout<<"Found "<<files.size()<<" files"<<endl;
     
-    //build workers
-    for(string machine : machines){
-        worker mworker;
-        mworker.machine = machine;
-        mworker.busy = false;
-        workers.push_back(mworker);
-    }
-    if(v) cout<<"Number of workers "<<workers.size()<<endl;
 
-
-//  TODO: send each file to a worker and send the fsdiff command
    while(!files.empty()){
-       //chop off the new line character
+       /*chop off the new line character*/
        string file(files.front().begin(), files.front().end()-1);
        
        worker *mworker = findAvailableWorker();
-       cout<<"REFERENCE: "<<mworker<<endl;
        if(v) cout<<mworker->machine<<" available, sending it file: "<<file<<endl;
        
        //TODO: file may not exist in other machine (also may have _original already appended to it)
        string cmdSsh = "ssh " + mworker->machine + " ";
        string cmdSshf ="ssh -f " + mworker->machine + " ";
        string cmdRenameFile = "'mv /" + file + " /" + file + "_original'";
-       //recursive copy maintins original creation dates
        string cmdCopyFile = "scp -rp /" + file + " root@"+ mworker->machine + ":/";
        //double check trailing & with a bigger file
        string cmdFsdiff = "'cd / ; ulimit -n 1024; /usr/local/bin/fsdiff -CIcsha1 -o "+ file + ".T ./" + file + "' &";
@@ -159,21 +137,78 @@ void manageWorkers(vector<string> machines){
        execute(cmdSshf + cmdFsdiff, false);
        files.pop();
        mworker->busy = true;
-       findAvailableWorker();
- 
-       return;//testing, only do once
+       mworker->file = file;
    }
     
 }
 
+string tmp_file = "/tmp/dist_fsdiff";
+/*Appends '/dist_fsdiff' to every line in the /var/radmind/.../transcripts for safe copying*/
+void processTrancripts(){
+    execute("mkdir "+tmp_file, IGNORE_RESPONCE);
+    string line;
+    ifstream transcript_in("/var/radmind/client/applications/content-creation/joe-3_7.T");
+    ofstream transcript_out("/tmp/joe-3_7.T");
+    if(transcript_in.is_open() && transcript_out.is_open()){
+        while(getline(transcript_in, line)){
+            size_t dotPosition = line.find(".");
+            line.insert(dotPosition +1 , tmp_file);
+            transcript_out << line <<endl;
+             
+        }
+        transcript_in.close();
+        transcript_out.close();
+    } else{
+        if(!transcript_out.is_open()){
+            cout<<"Because of oftream"<<endl;
+        }
+        if(!transcript_in.is_open()){
+            cout<<"becuase of ifstream"<<endl;
+        }
+    }
+    while(1);//hang here for testing
+}
 
-//TOOD: The authenticity of host... yes/no message
+void fsdiff_mr(){
+    processTrancripts();
+    
+    /*replace client folder in all target machines*/
+    for(worker mworker: workers){
+        string cmdSsh = "ssh " + mworker.machine + " ";
+        string cmdRenameClient = "'mv /var/radmind/client /var/radmind/client_original'";
+        string cmdCopyClient = "scp -r /var/radmind/client root@" + mworker.machine + ":/var/radmind/client";
+        
+        if(v) cout<<"Replacing /var/radmind/client of "<< mworker.machine <<endl;
+        execute(cmdSsh + cmdRenameClient, IGNORE_RESPONCE);
+        execute(cmdCopyClient, IGNORE_RESPONCE);
+        
+    }
+    if(v)cout<<"Creating working directory /transcripts"<<endl;
+    string cmdMakeWorkspace = "mkdir /transcripts";
+    execute(cmdMakeWorkspace, IGNORE_RESPONCE);
+    
+    
+    manageWorkers();
+    
+    /*Restore client folder in target machines*/
+    for(worker mworker : workers){
+        string cmdSsh = "ssh " + mworker.machine + " ";
+        string cmdRestoreClient = "'mv /var/radmind/client_original /var/radmind/client'";
+        string cmdDeleteWorkingDirectory = "rm -rf /transcripts";
+        
+        if(v) cout<<"Restoring original client folder on "<<mworker.machine<<endl;
+        execute(cmdSsh + cmdRestoreClient, IGNORE_RESPONCE);
+        execute(cmdDeleteWorkingDirectory, IGNORE_RESPONCE);
+    }
+}
+
+
+//TODO: The authenticity of host... yes/no message
 int main(int argc, const char * argv[]) {
     if(argc < 2) {
         cout<<"Please pass in at least one rdev-machine"<<endl;
         return -1;
     }
-    //read flags
     int i = 1;
     for(; i < argc && *argv[i] == '-'; i++){
         switch(argv[i][1]){
@@ -185,40 +220,15 @@ int main(int argc, const char * argv[]) {
             }
         }
     }
-    //get list of rdev machines
-    vector<string> machines;
     for(; i < argc; i++){
-        machines.push_back(argv[i]);
+        worker mworker;
+        mworker.machine = argv[1];
+        mworker.busy = false;
+        workers.push_back(mworker);
     }
+    if(v) cout<<"Number of workers "<<workers.size()<<endl;
     
-    //replace client
-    for(string machine: machines){
-        string cmdSsh = "ssh " + machine + " ";
-        string cmdRenameClient = "'mv /var/radmind/client /var/radmind/client_original'";
-        string cmdCopyClient = "scp -r /var/radmind/client root@" + machine + ":/var/radmind/client";
-        
-        if(v) cout<<"Replacing /var/radmind/client of "<< machine <<endl;
-        execute(cmdSsh + cmdRenameClient, IGNORE_RESPONCE);
-        execute(cmdCopyClient, IGNORE_RESPONCE);
-
-    }
-    if(v)cout<<"Creating working directory /transcripts"<<endl;
-    string cmdMakeWorkspace = "mkdir /transcripts";
-    execute(cmdMakeWorkspace, IGNORE_RESPONCE);
-    
-    
-    manageWorkers(machines);
-    
-    //cleanup
-    for(string machine : machines){
-        string cmdSsh = "ssh " + machine + " ";
-        string cmdRestoreClient = "'mv /var/radmind/client_original /var/radmind/client'";
-        string cmdDeleteWorkingDirectory = "rm -rf /transcripts";
-        
-        if(v) cout<<"Restoring original client folder on "<<machine<<endl;
-        execute(cmdSsh + cmdRestoreClient, IGNORE_RESPONCE);
-        execute(cmdDeleteWorkingDirectory, IGNORE_RESPONCE);
-    }
+    fsdiff_mr();
 
     return 0;
 }
